2340a30ef4ac9e15ae2aeb214d7d38be8a8a0055
mindmaster17 <127671399+mindmaster17@users.noreply.github.com>
2023-11-12 23:04:51 +0530

    Update database.js

--- api/database.js
+++ api/database.js
@@ -1,6 +1,32 @@
-import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js";
+// Import the functions you need from the SDKs you need
+	  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+	  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-analytics.js";
+	  import { getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, signInWithRedirect } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+	  import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js";
 
-const db = getDatabase();
+	  // TODO: Add SDKs for Firebase products that you want to use
+	  // https://firebase.google.com/docs/web/setup#available-libraries getDatabase
+
+	  // Your web app's Firebase configuration
+	  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+	  const firebaseConfig = {
+    	  apiKey: "AIzaSyCkNJFimfRBFgwiFRCNDtTPlo8g-2JIc-M",
+   	  authDomain: "login-page-c3c64.firebaseapp.com",
+   	  databaseURL: "https://login-page-c3c64-default-rtdb.asia-southeast1.firebasedatabase.app",
+          projectId: "login-page-c3c64",
+          storageBucket: "login-page-c3c64.appspot.com",
+          messagingSenderId: "426921518510",
+          appId: "1:426921518510:web:98a3252f91b0fdeaa5943f",
+          measurementId: "G-SKKHTLF0BH"
+	  };
+
+	  // Initialize Firebase
+	  const app = initializeApp(firebaseConfig);
+	  const analytics = getAnalytics(app);
+	  const auth = getAuth();
+ 	  const provider = new GoogleAuthProvider()
+	  console.log(app);
+    const db = getDatabase();
 
 // Function to add a score to a user
 const addScoreToUser = (userId, score) => {
